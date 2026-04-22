65420997c6227b377a2dee0396f13cb2243212c3
Kriththigan <kriththiganklp10@gmail.com>
2026-04-22 20:03:41 +0530

    Add LoggingFliter.java

---
+++ src/main/java/com/smartcampus/resource/LoggingFilter.java
@@ -0,0 +1,31 @@
+package com.smartcampus.resource;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+@Provider
+public class LoggingFilter implements ContainerRequestFilter, ContainerResponseFilter {
+
+    private static final Logger logger = Logger.getLogger(LoggingFilter.class.getName());
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        logger.info("Incoming Request: [" + 
+            requestContext.getMethod() + "] " + 
+            requestContext.getUriInfo().getRequestUri().toString());
+    }
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, 
+                       ContainerResponseContext responseContext) throws IOException {
+        logger.info("Outgoing Response: Status [" + 
+            responseContext.getStatus() + "] for [" +
+            requestContext.getMethod() + "] " +
+            requestContext.getUriInfo().getRequestUri().toString());
+    }
+}
