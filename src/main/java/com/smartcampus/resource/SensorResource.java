900db4ba52a3e7b68b137ef231d75eed6d9cf280
Kriththigan <kriththiganklp10@gmail.com>
2026-04-22 22:12:09 +0530

    Updated SensorResource.java

--- src/main/java/com/smartcampus/resource/SensorResource.java
+++ src/main/java/com/smartcampus/resource/SensorResource.java
@@ -10,7 +10,7 @@ import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.List;
 
-@Path("/sensors")
+@Path("/api/v1/sensors")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class SensorResource {
