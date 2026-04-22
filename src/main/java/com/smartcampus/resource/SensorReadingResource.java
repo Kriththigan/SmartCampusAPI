4f460426159f83571c5a84738c9de6994ebb3084
Kriththigan <kriththiganklp10@gmail.com>
2026-04-22 20:10:40 +0530

    Add SensorReadingResource.java

---
+++ src/main/java/com/smartcampus/resource/SensorReadingResource.java
@@ -0,0 +1,70 @@
+package com.smartcampus.resource;
+
+import com.smartcampus.exception.SensorUnavailableException;
+import com.smartcampus.model.Sensor;
+import com.smartcampus.model.SensorReading;
+import com.smartcampus.store.DataStore;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class SensorReadingResource {
+
+    private final DataStore store = DataStore.getInstance();
+    private final String sensorId;
+
+    public SensorReadingResource(String sensorId) {
+        this.sensorId = sensorId;
+    }
+
+    @GET
+    public Response getReadings() {
+        Sensor sensor = store.getSensor(sensorId);
+
+        if (sensor == null) {
+            return Response.status(Response.Status.NOT_FOUND)
+                    .entity("{\"error\":\"Sensor not found with ID: " + sensorId + "\"}")
+                    .build();
+        }
+
+        List<SensorReading> readings = store.getReadings(sensorId);
+        return Response.ok(readings).build();
+    }
+
+    @POST
+    public Response addReading(SensorReading reading) {
+        Sensor sensor = store.getSensor(sensorId);
+
+        if (sensor == null) {
+            return Response.status(Response.Status.NOT_FOUND)
+                    .entity("{\"error\":\"Sensor not found with ID: " + sensorId + "\"}")
+                    .build();
+        }
+
+        if ("MAINTENANCE".equalsIgnoreCase(sensor.getStatus())) {
+            throw new SensorUnavailableException(
+                "Sensor '" + sensorId + "' is currently under MAINTENANCE " +
+                "and cannot accept new readings.");
+        }
+
+        if ("OFFLINE".equalsIgnoreCase(sensor.getStatus())) {
+            throw new SensorUnavailableException(
+                "Sensor '" + sensorId + "' is OFFLINE " +
+                "and cannot accept new readings.");
+        }
+
+        SensorReading newReading = new SensorReading(reading.getValue());
+
+        sensor.setCurrentValue(reading.getValue());
+
+        store.addReading(sensorId, newReading);
+
+        return Response.status(Response.Status.CREATED)
+                .entity(newReading)
+                .build();
+    }
+}
